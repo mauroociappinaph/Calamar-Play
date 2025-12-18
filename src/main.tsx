216f4f3cb38527e6198627e1200317c7f8463908
Mauro Ciappina <ciappinamaurooj@gmail.com>
2025-12-18 18:55:55 -0300

    test lint block

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/app/App';
 
+console.log('test');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
